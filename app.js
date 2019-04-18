001935ff728504e15a6c07c1139f613812ec59c8
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-04-18 21:03:27 +0300

    Add a new todos form properly work, adding a new lines with exact value from input, spaces are triming, form reseting.

---
+++ app.js
@@ -0,0 +1,30 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+// Function that create a new todo line, html template
+
+const generateTemplate = (todo) => {
+
+    const html = `
+         <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+
+    list.innerHTML += html;
+
+};
+
+addForm.addEventListener('submit', event => {
+    
+    event.preventDefault();
+
+    const todo = addForm.add.value.trim();
+
+    if(todo.length){
+        generateTemplate(todo);    
+        addForm.reset();    
+    }
+
+})
